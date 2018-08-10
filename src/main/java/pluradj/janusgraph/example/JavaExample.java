3f21708e0c3f1fc35460cc7679a71361c07cf331
Jason Plurad <pluradj@us.ibm.com>
2018-08-10 15:01:48 -0400

    JanusGraph issue 1211 https://github.com/JanusGraph/janusgraph/issues/1211
    Signed-off-by: Jason Plurad <pluradj@us.ibm.com>

--- src/main/java/pluradj/janusgraph/example/JavaExample.java
+++ src/main/java/pluradj/janusgraph/example/JavaExample.java
@@ -3,30 +3,44 @@ package pluradj.janusgraph.example;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.structure.Edge;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.janusgraph.core.Cardinality;
 import org.janusgraph.core.JanusGraph;
 import org.janusgraph.core.JanusGraphFactory;
-import org.janusgraph.core.attribute.Geo;
-import org.janusgraph.core.attribute.Geoshape;
-import org.janusgraph.example.GraphOfTheGodsFactory;
+import org.janusgraph.core.PropertyKey;
+import org.janusgraph.core.VertexLabel;
+import org.janusgraph.core.schema.JanusGraphManagement;
+import org.janusgraph.core.schema.PropertyKeyMaker;
+import org.janusgraph.core.schema.VertexLabelMaker;
+import org.janusgraph.graphdb.database.management.ManagementSystem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.List;
-import java.util.Map;
 
 public class JavaExample {
     private static final Logger LOGGER = LoggerFactory.getLogger(JavaExample.class);
 
     public static void main(String[] args) {
-        JanusGraph graph = JanusGraphFactory.open("conf/janusgraph-berkeleyje-lucene.properties");
-        GraphTraversalSource g = graph.traversal();
-        if (g.V().count().next() == 0) {
-            // load the schema and graph data
-            GraphOfTheGodsFactory.load(graph);
+        JanusGraph janusGraph = JanusGraphFactory.open("conf/janusgraph-cassandra.properties");
+
+        JanusGraphManagement graphManagement = janusGraph.openManagement();
+        VertexLabelMaker vertexLabelMaker = graphManagement.makeVertexLabel("user");
+        vertexLabelMaker.make();
+        PropertyKeyMaker propertyKeyMaker = graphManagement.makePropertyKey("user_id");
+        propertyKeyMaker.dataType(Long.class).cardinality(Cardinality.SINGLE).make();
+        graphManagement.commit();
+
+        graphManagement = janusGraph.openManagement();
+        PropertyKey userId = graphManagement.getPropertyKey("user_id");
+        VertexLabel vertexLabel = graphManagement.getVertexLabel("user");
+        graphManagement.buildIndex("my_id_index", Vertex.class).addKey(userId).indexOnly(vertexLabel).buildCompositeIndex();
+        graphManagement.commit();
+
+        try {
+            ManagementSystem.awaitGraphIndexStatus(janusGraph, "my_id_index").call();
+        } catch (InterruptedException e) {
+            LOGGER.error(e.getMessage(), e);
         }
-        Map<Object, Object> saturnProps = g.V().has("name", "saturn").valueMap(true).next();
-        LOGGER.info(saturnProps.toString());
-        List<Edge> places = g.E().has("place", Geo.geoWithin(Geoshape.circle(37.97, 23.72, 50))).toList();
-        LOGGER.info(places.toString());
+
+        janusGraph.close();
         System.exit(0);
     }
 }
